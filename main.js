23fd84d31d69402de4588ece2f1a24275ac59883
苏火强 <suhuoqiang@suhuoqiangdeMacBook-Pro.local>
2017-10-26 23:46:42 +0800

    添加下参考链接

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
+//https://juejin.im/entry/57d906ee79bc440065d276a1
 let startstamp = Date.parse(new Date());
-console.log(selectionSort(random(26,30000,10000)));
+console.log(selectionSort_1(random(26,30000,10000)));
 let stopstamp = Date.parse(new Date());
 console.log(stopstamp-startstamp+'ms');
--- selectionSort.js
+++ selectionSort.js
@@ -2,7 +2,22 @@
  * 第一种选择排序算法
  * @param {*} arr 
  */
-var selectionSort = (arr) =>{
+var selectionSort_1 = (arr) =>{
+    let len =arr.length;
+    for(let i=0;i<len;i++){
+        for(let j=0;j<len-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+/**
+ * 第二种选择排序
+ * @param {*} arr 
+ */
+var selectionSort_2 = (arr) =>{
     let len =arr.length;
     for(let i=0;i<len;i++){
         for(let j=0;j<len-1-i;j++){
