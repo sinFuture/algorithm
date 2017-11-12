e8a0e3c2ec2bf62bbec9cf49207ff3cefb8a4ccb
苏火强 <suhuoqiang@suhuoqiangdeMacBook-Pro.local>
2017-11-12 09:11:41 +0800

    误将冒泡的算法写成了选择排序了

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 //https://juejin.im/entry/57d906ee79bc440065d276a1
-let startstamp = Date.parse(new Date());
-console.log(selectionSort_1(random(26,30000,10000)));
-let stopstamp = Date.parse(new Date());
+console.log("第一种算法：");
+var startstamp = Date.parse(new Date());
+console.log(bubbleSort_1(random(26,30000,10000)));
+var stopstamp = Date.parse(new Date());
+console.log(stopstamp-startstamp+'ms');
+
+
+console.log("第二种算法：");
+var startstamp = Date.parse(new Date());
+console.log(bubbleSort_2(random(26,30000,10000)));
+var stopstamp = Date.parse(new Date());
 console.log(stopstamp-startstamp+'ms');
